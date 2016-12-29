5da9721bd778818b85f60aad4aec5c71f292d8e0
Jiongxiao <tctjx@hotmail.com>
2016-12-29 18:03:18 +0800

    add batchnorm

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -606,7 +606,11 @@ def spatial_batchnorm_forward(x, gamma, beta, bn_param):
   # version of batch normalization defined above. Your implementation should  #
   # be very short; ours is less than five lines.                              #
   #############################################################################
-  pass
+  N,C,H,W=x.shape
+  x=x.transpose(0,2,3,1)
+  x=x.reshape(N*H*W,C)
+  out,cache=batchnorm_forward(x,gamma,beta,bn_param)
+  out=out.reshape(N,H,W,C).transpose(0,3,1,2)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -636,7 +640,10 @@ def spatial_batchnorm_backward(dout, cache):
   # version of batch normalization defined above. Your implementation should  #
   # be very short; ours is less than five lines.                              #
   #############################################################################
-  pass
+  N,C,H,W=dout.shape
+  dout=dout.transpose(0,2,3,1).reshape(N*H*W,-1)
+  dx,dgamma,dbeta=batchnorm_backward(dout,cache)
+  dx=dx.reshape(N,H,W,C).transpose(0,3,1,2)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
